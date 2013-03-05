7f2ef16e87afb1a5313f6b65a8fb594936853e12
antanariva <antanariva@gmai.com>
2013-03-05 13:14:58 +0600

    копия с ноута notebook ветки

--- theme/ITDlab.theme/rightcolumn.itd.php
+++ theme/ITDlab.theme/rightcolumn.itd.php
@@ -6,17 +6,20 @@
 *****************************************************/
 ?>
 <div id="submenu" align="right"><!-- Подменю сайтас сверху -->
-	<a href="news.html" title="Новые публикации" alt="Новые публикации">Новые публикации</a>&nbsp;|&nbsp;<a href="med.html" title="Медицина" alt="Медицина">Медицина</a>&nbsp;|&nbsp;<a href="it.html" title="IT-технологии" alt="IT-технологии">IT-технологии</a>&nbsp;|&nbsp;<a href="civ.html" title="Цивилизация. XXI век" alt="Цивилизация. XXI век">Цивилизация. XXI век</a>&nbsp;|&nbsp;<a href="gallery.html" title="Фото и видео галереи" alt="Фото и видео галереи">Фото и видео</a>&nbsp;|&nbsp;<a href="autors.html" title="Об авторах" alt="Об авторах">Об авторах</a>
+	<a href="news.html" title="Новые публикации" alt="Новые публикации">Новые публикации</a>&nbsp;|&nbsp;<a href="<?php get_site_url(); ?>voprosi-medicini/" title="Медицина" alt="Медицина">Медицина</a>&nbsp;|&nbsp;<a href="it.html" title="IT-технологии" alt="IT-технологии">IT-технологии</a>&nbsp;|&nbsp;<a href="civ.html" title="Цивилизация. XXI век" alt="Цивилизация. XXI век">Цивилизация. XXI век</a>&nbsp;|&nbsp;<a href="gallery.html" title="Фото и видео галереи" alt="Фото и видео галереи">Фото и видео</a>&nbsp;|&nbsp;<a href="autors.html" title="Об авторах" alt="Об авторах">Об авторах</a>
 </div>
-
-<aside id="sidebar">  
-	<div id="news"><!-- Правый блок с новыми статьями -->
-	     <?php get_component('sidebar');	?>
-    </div>
-</aside>
 	
-<aside id="sidebar">  
-	<div id="news"><!-- Правый блок ещё с чем-нибудь -->
-	    <?php get_component('tagline');	?>
+<aside>  
+	<div id="nav2"><!-- Правый блок ещё с чем-нибудь -->	
+	    <?php get_i18n_navigation(return_page_slug(), 1, 99); ?>
     </div>
 </aside>
+
+<aside>  
+	<div id="news"><!-- Правый блок с новыми статьями -->
+		 <?php get_component('tagline'); ?>
+    </div>
+</aside>
+
+
+
